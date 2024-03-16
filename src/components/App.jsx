077adc608d138231b1eb681f8ee3996d7514e6af
Ukro <koliaso@gmail.com>
2024-03-16 21:40:52 +0200

    wordlaw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,41 @@
+import i18n from 'i18next';
+import { useTranslation } from 'react-i18next';
+
+// Initialize i18next
+i18n.init({
+  resources: {
+    en: {
+      translation: {
+        welcome: 'Welcome to our website!',
+        about: 'About Us',
+        contact: 'Contact Us',
+      },
+    },
+    uk: {
+      translation: {
+        welcome: 'Вітаємо на нашому сайті!',
+        about: 'Про нас',
+        contact: 'Звязатися з нами',
+      },
+    },
+  },
+  lng: 'uk', // Default language
+  fallbackLng: 'en',
+});
+
+
+
 export const App = () => {
+  // Translate text
+
+const { t } = useTranslation();
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+    <h1>{t('welcome')}</h1>
+    <p>
+      <a href="/about">{t('about')}</a> | <a href="/contact">{t('contact')}</a>
+    </p>
     </div>
   );
 };
